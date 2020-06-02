468260a2652f3521dd67b7dcb47f7d3f94393994
frasfras <65541080+frasfras@users.noreply.github.com>
2020-06-02 11:11:07 +0800

    Update surveyRoute.js
    yes /no ..

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -15,11 +15,45 @@ module.exports = app =>{
     res.send('Thanks for voting!');
   });
 
+     app.post('/api/surveys/webhooks', (req, res) => {
+    console.log('hello');
+       const p = new Path('api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
+         res.send({});
+  });  
+	
+	
 	app.post('/api/surveys',requireLogin,requireCredits, async (req,res) => {
 			const { title, subject ,body, recipients} = req.body;
 
 
-      const survey = new Survey({
+         const survey = new Survey({
         
       	 title, subject,body,
          // recipients: 'litvil77@gmail.com,',
